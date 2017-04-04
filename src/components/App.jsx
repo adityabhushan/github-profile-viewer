9cb3f24bf5e211cdd8fd478f0c8990549fba3586
Aditya <aditya@rangde.org>
2017-04-04 16:06:36 +0530

    Complete search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import Profile from './github/Profile.jsx'
+import Profile from './github/Profile.jsx';
+import Search from './github/Search.jsx';
 
 class App extends Component {
 	constructor(props) { 
@@ -20,8 +21,7 @@ class App extends Component {
 			dataType: 'json',
 			cache: false,
 			success: function(data){
-				this.setState({userData: data});
-				console.log(data)
+				this.setState({userData: data});				
 			}.bind(this),
 			error: function(xhr, status, err) {
 				this.setState({username: null});
@@ -30,15 +30,41 @@ class App extends Component {
 		});
 	}
 
+
+	//Get user Repos from github
+	getUserRepos (){
+		$.ajax({
+			url: 'https://api.github.com/users/'+this.state.username+'/repos?per_page='+this.state.perPage+'&client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret+'&sort=created',
+			dataType: 'json',
+			cache: false,
+			success: function(data){
+				this.setState({userRepos: data});				
+			}.bind(this),
+			error: function(xhr, status, err) {
+				this.setState({username: null});
+				alert(err)
+			}.bind(this)
+		});
+	}
+
+	handleFormSubmit(username){
+		this.setState({username: username}, function(){
+			this.getUserData();
+			this.getUserRepos();
+		})
+	}
+
 	componentDidMount() {
 		this.getUserData();
+		this.getUserRepos();
 	}
 
 
 	render() {
 		return(
 			<div>
-				<Profile userData = {this.state.userData}/> 
+				<Search onFormSubmit = {this.handleFormSubmit.bind(this)}/>
+				<Profile {...this.state}/> 
 			</div> 
 			)
 	}
